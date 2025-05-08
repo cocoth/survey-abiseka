fed1a0ef66bf8037d49dee9976df4eb588742c8a
cocoth <anonymousid0000000@gmail.com>
2025-05-08 23:58:51 +0700

    edit layout

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -5,6 +5,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="flex">
       <Sidebar />
       <div className="flex-1 p-4">{children}</div>
+      {/* <div className="flex-1">
+        <Navbar />
+        <main className="p-4">
+          {children}
+        </main>
+      </div> */}
+
     </div>
   );
 }
